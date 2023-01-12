59e6f4dcc2134a81e9c67f0a4995c07a2f2830ca
ruchith696 <ruchithreddy21@gmail.com>
2023-01-12 13:41:17 +0530

    rendered data coming from database

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./App.css";
 import AppHeader from "./components/AppHeader";
 import BodyCard from "./components/BodyCard";
+import { Employee } from "./services/employee";
+import { EmployeeService } from "./services/employee.service";
 
 function App() {
+  const employeeService = new EmployeeService();
+  const [employees, setEmployees] = useState<Employee[]>([]);
+  employeeService.getEmployeees().then((value) => {
+    setEmployees(value);
+  });
   return (
     <>
       <AppHeader />
-      <BodyCard />
+
+      <BodyCard employees={employees} />
     </>
   );
 }
---
+++ src/components/EmployeeCard.tsx
@@ -0,0 +1,96 @@
+import * as React from "react";
+
+import { Button, ButtonGroup, Card, Image, ListGroup } from "react-bootstrap";
+import { Employee } from "../services/employee";
+
+interface IEmployeeCardProps {
+  employee: Employee;
+}
+
+const EmployeeCard: React.FunctionComponent<IEmployeeCardProps> = (props) => {
+  return (
+    <Card className="card">
+      <Card.Body className="row">
+        <div className="col-6">
+          <a href="">
+            <Image
+              src={props.employee.imageUrl}
+              roundedCircle
+              className="w-60"
+            />
+          </a>
+        </div>
+
+        <div className="col-6 mb-0">
+          <Card.Title className="align-self-center">
+            {props.employee.name}
+          </Card.Title>
+          <p className="m-0">{props.employee.jobTitle}</p>
+        </div>
+
+        <ListGroup variant="flush">
+          <ListGroup.Item>
+            <i className="fa fa-envelope float-end"></i>Email :{" "}
+            <a href="#">{props.employee.email}</a>
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <i className="fa fa-phone float-end"></i>Phone :{" "}
+            {props.employee.phone}
+          </ListGroup.Item>
+        </ListGroup>
+      </Card.Body>
+      <Card.Body>
+        <ButtonGroup className="float-end" size="sm">
+          <Button variant="primary">
+            <i className="fa fa-pencil"></i>{" "}
+          </Button>
+          <Button variant="secondary">
+            <i className="fa fa-times"></i>
+          </Button>
+        </ButtonGroup>
+        <div className="social-links">
+          <ListGroup horizontal>
+            <ListGroup.Item>
+              <a
+                title=""
+                data-placement="top"
+                data-toggle="tooltip"
+                className="tooltips"
+                href=""
+                data-original-title="Facebook"
+              >
+                <i className="fa fa-facebook-f"></i>
+              </a>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <a
+                title=""
+                data-placement="top"
+                data-toggle="tooltip"
+                className="tooltips"
+                href=""
+                data-original-title="Twitter"
+              >
+                <i className="fa fa-twitter"></i>
+              </a>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <a
+                title=""
+                data-placement="top"
+                data-toggle="tooltip"
+                className="tooltips"
+                href=""
+                data-original-title="Skype"
+              >
+                <i className="fa fa-skype"></i>
+              </a>
+            </ListGroup.Item>
+          </ListGroup>
+        </div>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default EmployeeCard;
